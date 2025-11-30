99922ad484b36cad493a1e627f8a2cf7c512ec2e
utwoa <loskon64@gmail.com>
2025-11-30 23:01:58 +0300

    upd

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import uuid
 import os
 import logging
 from datetime import timedelta
-from fastapi import FastAPI, Request, Form, Query, Header
+from fastapi import FastAPI, Request, Form, Query, Header, HTTPException
 from pydantic import BaseModel
 from zoneinfo import ZoneInfo
 import httpx
